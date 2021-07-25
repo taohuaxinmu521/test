e989912c04ad4f3609e05c4f7ed2ae135bfc4cc7
tanwen <690387060@qq.com>
2021-07-25 13:57:07 +0800

    test

---
+++ test-git/spring-test/src/main/java/org/spring/test/TestApp.java
@@ -0,0 +1,17 @@
+package org.spring.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class TestApp 
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(TestApp.class, args);
+    }
+}
